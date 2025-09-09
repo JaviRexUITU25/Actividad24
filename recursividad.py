7b09f261c4be971826ae7d8ebdb9b83f79618ee7
Javier Huertas <javiandre2007@gmail.com>
2025-09-09 09:37:01 -0600

    menu y opcion 4

--- recursividad.py
+++ recursividad.py
@@ -21,4 +21,36 @@ def contar_letra(text):
     if len(text()):
         pass
 while True:
-    print("hola")
+    print("-"*10 + "MENU" + "-"*10 + "\n"
+          "1. Calcular el factorial de un numero\n"
+          "2. Suma de los primeros numeros naturales\n"
+          "3. Calcular el numero de fibonacci\n"
+          "4. Cuantas veces aparece una letra en una palabra\n"
+          "5. Invertir cadena de un texto\n"
+          "6. Calcular la potencia de un número\n"
+          "7. Salir")
+    option = int(input("Ingrese la opcion que desea consultar: "))
+    match option:
+        case 1:
+            n = int(input("Ingrese un número: "))
+            factorial()
+            print(f"El factorial de {n} es: {factorial(n)}")
+        case 2:
+            n = int(input("Ingrese un número: "))
+            suma_entero()
+            print(f"La suma de {n} es: {factorial(n)}")
+        case 3:
+            n = int(input("Ingrese un numero: "))
+            fibonacci()
+            print(f"El numero en fibonacci es: {fibonacci(n)}")
+        case 4:
+            n = int(input("Ingrese una palabra"))
+
+        case 5:
+            pass
+        case 6:
+            pass
+        case 7:
+            print("Saliendo del programa...")
+        case _:
+            print("Ingrese una opcion valida")
